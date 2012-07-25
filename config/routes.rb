ede8d2acc482a9d402c4afcf480617792f3fd4d1
Shaun Robinson <shaun@squiid.com>
2012-07-25 09:15:14 -0400

    Added artists, orders to routes and newsletter

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Tix::Application.routes.draw do
 
+
   root :to => "main#index"
+  
+  get '/newsletter' => 'newsletter#index'
 
   scope '/api' do
     resources :events
+    resources :artists
+    resources :artists
+    resources :orders
+    
+    match '/stats/:action', :controller => :stats
+    
     get '/ticket_locks.json/new' => "ticket_locks#new"
     get '/ticket_locks.json/delete' => "ticket_locks#destroy"
     post '/ticket_locks.json/new' => "ticket_locks#new"

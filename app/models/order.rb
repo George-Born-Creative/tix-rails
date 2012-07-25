4cc76c490886adf6302d6e6d911f6f73905d5765
Shaun Robinson <shaun@squiid.com>
2012-07-25 09:54:57 -0400

    Removed ticket ids flat field from order -- not yet ready

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,4 @@
 class Order < ActiveRecord::Base
   has_many :tickets
   
-  attr_accessible :ticket_ids_flat
-  
-  before_save :process_tickets
-  
-  def process_tickets
-    ticket_ids_flat = self.ticket_ids_flat
-    ticket_ids_flat = JSON.parse ticket_ids_flat
-  end
-  
 end

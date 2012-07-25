f8715ed03c296102616275e0c7f85f1e4c779ee7
Shaun Robinson <shaun@squiid.com>
2012-07-25 09:14:35 -0400

    Added US East as main timezone

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Tix
     config.assets.initialize_on_precompile = false
     config.action_mailer.delivery_method   = :postmark
     config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
-
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

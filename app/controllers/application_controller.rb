d5c8041d3d24871c478f874c9a6f71f7b5d9c981
Shaun Robinson <shaun@squiid.com>
2012-07-31 10:47:42 -0400

    Login implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout
+  
   before_filter :set_event
+  before_filter :authenticate_user!
   
   def set_event
     @events = Event.order("starts_at ASC")
   end
+  
+  
+  private
+
+  def layout
+    if devise_controller? || resource_name == :user || action_name == 'sign_in' || action_name == 'sign_up'
+     "login"
+    else
+     "application"
+    end  
+  end
+
+
+
 end

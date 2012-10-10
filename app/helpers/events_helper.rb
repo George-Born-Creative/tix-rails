eeb53f86406c1317b30d6e557b907f9fba879ea0
Shaun Robinson <shaun@squiid.com>
2012-10-10 12:26:49 -0400

    Added social bar and helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,9 +5,16 @@ module EventsHelper
     
     opts.reverse_merge!(defaults)
     
-    code =  /embed\/([a-zA-Z0-9]+)/.match(embed_url)[1]
+    code =  /embed\/([a-zA-Z0-9]+)/.match(embed_url)
     
-    "<iframe width='#{opts[:width]}' height='#{opts[:height]}' src='http://www.youtube.com/embed/#{code}' frameborder='0' allowfullscreen></iframe>".html_safe
+    unless code.nil? || code.empty? || code[1].nil?
+      "<iframe width='#{opts[:width]}' height='#{opts[:height]}' src='http://www.youtube.com/embed/#{code[1]}' frameborder='0' allowfullscreen></iframe>".html_safe
+    end
   end
-    
+   
+   
+   def artist_social_bar(artist)
+     render :partial => 'front/artists/social_bar', :locals => {:artist => artist}
+   end 
+   
 end

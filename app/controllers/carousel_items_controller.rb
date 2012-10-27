dd7f55808dbd3f267a6c4f023ddd927a8ae06ef2
Shaun Robinson <shaun@squiid.com>
2012-10-26 23:04:49 -0400

    More work on carousel'

--- app/controllers/carousel_items_controller.rb
+++ app/controllers/carousel_items_controller.rb
@@ -2,7 +2,7 @@ class CarouselItemsController < InheritedResources::Base
   layout 'manager_cms'
   
   
-  before_filter :set_carousel, :only => [:index, :new, :create, :update, :edit]
+  before_filter :set_carousel, :only => [:index, :new, :create, :update, :edit, :destroy]
   
   def index
     @carousel_items = @carousel.carousel_items
@@ -10,15 +10,17 @@ class CarouselItemsController < InheritedResources::Base
 
   def new
     @carousel_item = @carousel.carousel_items.new
+    @carousel_item.build_image unless @carousel_item.image
+    
   end
   
   def edit
     @carousel_item = @carousel.carousel_items.find(params[:id])
-    @carousel_item.build_image
+    @carousel_item.build_image unless @carousel_item.image
   end
   
   def create
-    @carousel_item = @carousel.carousel_items.build(params[:review])  
+    @carousel_item = @carousel.carousel_items.build(params[:carousel_item])  
     if @carousel_item.save  
       flash[:notice] = "Successfully created carousel item."  
       redirect_to carousel_path(@carousel)  
@@ -37,11 +39,22 @@ class CarouselItemsController < InheritedResources::Base
     end
   end
   
+  def destroy
+    @carousel_item = @carousel.carousel_items.find(params[:id]) 
+    @carousel_item.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to carousel_url(@carousel) }
+      format.json { head :no_content }
+    end
+
+  end
   
   private
   
   def set_carousel
     @carousel = Carousel.find(params[:carousel_id])
   end
+  
   
 end

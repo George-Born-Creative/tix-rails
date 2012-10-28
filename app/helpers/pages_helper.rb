84c8d1ba053b04f8056af8ac56275c4de4dd9054
Shaun Robinson <shaun@squiid.com>
2012-10-27 20:44:28 -0400

    Carousel

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -9,18 +9,30 @@ module PagesHelper
         # 
         
   
-  
-  
   def render_shortcodes(html)
     html = apply_events_list(html)
+    html = apply_carousels(html)
+  end
+  
+  
+  def apply_carousels(html)
+   regex = /\[(carousel) name=\&[a-z]+;([a-z0-9|]+[|]?)+\&[a-z]+;\]/
+
+    html.gsub!(regex) do |match|
+      slug = $2
+      @carousel = Carousel.find_by_slug(slug)
+      render(:partial => 'carousels/carousel_full', :locals => {:carousel => @carousel })
+    end
+
+    html
   end
   
   
   def apply_events_list(html)
     #events_list_regex = /\[events_list cat=\&[a-z]+;([a-z0-9]+)\&[a-z]+;\]/
-    events_list_regex = /\[(events_list) cat=\&[a-z]+;([a-z0-9|]+[|]?)+\&[a-z]+;\]/
+    regex = /\[(events_list) cat=\&[a-z]+;([a-z0-9|]+[|]?)+\&[a-z]+;\]/
 
-    html.gsub!(events_list_regex) do |match|
+    html.gsub!(regex) do |match|
       cats = $2.split('|').map{|c| c.to_sym}
       @events = @current_account.events.cat(cats).current.order('starts_at ASC')      
       render(:partial => 'front/events/events', :locals => {:events => @events, :cat => 'C' })

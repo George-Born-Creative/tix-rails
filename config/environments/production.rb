0ff7f663a334144e646add40f6c92f5090f4e170
Shaun Robinson <shaun@squiid.com>
2012-10-29 19:06:40 -0400

    Fix port in production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,21 @@ Tix::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+    
   
-  config.action_mailer.delivery_method   = :postmark
-  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
-  
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"]
+  }
+  # …
   
   config.after_initialize do
     ActiveMerchant::Billing::Base.mode = :test
   end
+  
+  
   
   
 end

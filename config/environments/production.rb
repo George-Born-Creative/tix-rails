206fc73d2accf64bd7705827b5f5eea75b4d01a0
Shaun Robinson <shaun@squiid.com>
2012-11-14 11:11:38 -0500

    Set action mailer default URL'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Tix::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
     
+  config.action_mailer.default_url_options = {:host => 'https://jamminjava.thintix.com'}
   
   config.action_dispatch.rack_cache = {
     :metastore    => Dalli::Client.new,

c85cd4e5d5b812f0f23c7228867867d6f5aaff6e
Shaun Robinson <shaun@squiid.com>
2012-11-14 23:46:31 -0500

    Adding custom registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+  layout 'front_user'
+  
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def update
+    super
+  end
+end

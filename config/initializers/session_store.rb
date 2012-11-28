929ad43103563d284ad33076e27f210da0126fdc
Shaun Robinson <shaun@squiid.com>
2012-11-27 20:40:05 -0500

    Switch to Memcached session store (via Dalli)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Tix::Application.config.session_store :cookie_store, key: '_tix_session'
+# Tix::Application.config.session_store :cookie_store, key: '_tix_session'
 
-# Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
-
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 60.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

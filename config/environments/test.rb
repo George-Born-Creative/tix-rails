e82666d978f257cc4f5a3982fc047e40e3039820
Shaun Robinson <shaun@squiid.com>
2012-12-07 09:23:55 -0500

    Enable ActveMerchant test mode for test.rb environment file
    Also logs depreciation notices to log

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,5 +33,11 @@ Tix::Application.configure do
   config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :log
+  
+  
+  config.after_initialize do
+    ActiveMerchant::Billing::Base.mode = :test 
+  end
+  
 end

2e10dd9274b88ac44ef44bfbe9e8be764eef8e7a
Shaun Robinson <shaun@squiid.com>
2012-12-19 12:18:54 -0500

    Fix cache path for event

--- app/controllers/front/events_controller.rb
+++ app/controllers/front/events_controller.rb
@@ -1,9 +1,5 @@
 class Front::EventsController < ApplicationController
   
-  caches_action :show, :expires_in => 15.minutes, :cache_path => proc {
-    "/events/#{params[:slug]}"
-  }
-  
   def index
     render :text => params[:id]
   end

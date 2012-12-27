039ade0b5daf82d081210fbe52fa4a0d932198f0
Shaun Robinson <shaun@squiid.com>
2012-12-27 18:29:24 -0500

    Add pagination to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < InheritedResources::Base
   respond_to :json, :html
 
   
+
   
   def tickets
     @tickets = Order.find(params[:id]).tickets
@@ -16,7 +17,11 @@ class OrdersController < InheritedResources::Base
   protected
   
   def collection
-    @orders ||= end_of_association_chain.complete.order('created_at DESC')
+    @orders ||= end_of_association_chain
+      .complete
+      .order('created_at DESC')
+      .page(params[:page] || 1)
+      .per(params[:per] || 10)
   end
   
   def begin_of_association_chain

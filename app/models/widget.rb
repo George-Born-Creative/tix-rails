b292f2cc6134e4986475ab6b122057394aa693b5
Shaun Robinson <shaun@squiid.com>
2013-01-06 11:34:25 -0500

    Cleanup widget model

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -20,19 +20,10 @@ class Widget < ActiveRecord::Base
   attr_accessible :body, :slug, :title, :account_id, :sidebar_ids
   
   validates_presence_of :title
-  #validates_uniqueness_of :slug, :scope => :account_id
+  # validates_uniqueness_of :slug, :scope => :account_id
   
   accepts_nested_attributes_for :sidebars
   
-  #  Accepts params[:sidebar_ids]  (e.g. from checkboxes)
-  # "sidebar_ids"=>["1", "2", ""]}, 
-  # def update_sidebars(sidebar_id_params)
-  #   Sidebar.each     
-  #     sidebar_id_params.each do |sidebar_id|
-  #       self.sidebars << Sidebar.find(sidebar_id)
-  #     end
-  #   end
-  # end
   
   
 end

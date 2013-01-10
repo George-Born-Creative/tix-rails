148580fca4c97a511b62c09c16612b01f4de7e9d
Shaun Robinson <shaun@squiid.com>
2013-01-10 15:04:17 -0500

    Reverse checkin polarity and add date

--- app/views/front/tickets/checkin_toggle.js
+++ app/views/front/tickets/checkin_toggle.js
@@ -1,5 +1,5 @@
 if (<%= @checked_in %>) {
-  $('.checkin-link-<%= @ticket.id %>').find('a').removeClass('btn-warning').parent().effect('highlight');
-} else {
   $('.checkin-link-<%= @ticket.id %>').find('a').addClass('btn-warning').parent().effect('highlight');
+} else {
+  $('.checkin-link-<%= @ticket.id %>').find('a').removeClass('btn-warning').parent().effect('highlight');
 }

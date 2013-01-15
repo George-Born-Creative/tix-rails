67ebda8c156279c154c7a30071f1677f96406dd9
Shaun Robinson <shaun@squiid.com>
2013-01-14 23:00:28 -0500

    Only show unannounced event to employees or preview link

--- app/controllers/front/events_controller.rb
+++ app/controllers/front/events_controller.rb
@@ -7,7 +7,12 @@ class Front::EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     
-    unless @event.announced? || @current_user && @current_user.has_at_least_role(:employee)
+    # If event is not announced, only show to employees
+    # or if preview params is yes
+    unless @event.announced? || 
+        (@current_user && @current_user.has_at_least_role(:employee)) || 
+        params[:preview] == 'yes'
+        
       not_found
     end 
       

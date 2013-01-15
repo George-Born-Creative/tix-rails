be5477bdaed235cf1d230d61acd5cb65cbab61ae
Shaun Robinson <shaun@squiid.com>
2013-01-14 22:49:28 -0500

    Ensure events list only includes announced events

--- app/controllers/front/events_controller.rb
+++ app/controllers/front/events_controller.rb
@@ -6,6 +6,11 @@ class Front::EventsController < ApplicationController
   
   def show
     @event = Event.find(params[:id])
+    
+    unless @event.announced? || @current_user && @current_user.has_at_least_role(:employee)
+      not_found
+    end 
+      
   end
   
   

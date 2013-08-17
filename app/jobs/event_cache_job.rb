3bd50b073fb528936145ecfe38b5ca500740b4a0
Shaun Robinson <shaun@squiid.com>
2013-08-17 14:46:23 -0400

    [app] Remove events cache jobs

--- app/jobs/event_cache_job.rb
+++
@@ -1,9 +0,0 @@
-require 'render_anywhere'
-
-class EventCacheJob < Struct.new(:event_id)
-  def perform
-    cache_service = EventCacheService.new(event_id)
-    cache_service.perform
-  end
-  
-end

048ef917341391b47df2b5f7ee97fe5ba89a20a1
Shaun Robinson <shaun@squiid.com>
2013-08-25 21:08:22 -0400

    [app] Add notes to cache warmer and prevent from loading in test env

--- config/initializers/cache_warming.rb
+++ config/initializers/cache_warming.rb
@@ -1,1 +1,19 @@
-Delayed::Job.enqueue PageCacheWarmingJob.new(1)
+# Notes on 'singleton' delayed_job in initializer 
+# (example: long-runing cache warming )
+# 
+# [SR] Aug 25, 2013
+# 
+# 1. Jobs will be enqueued by each worker/dyno instance
+#  
+# 2. When processes exit, jobs will still be on the queue.
+#
+# Current solution: 'last man wins'
+# Each enqueued job destroys existing 
+# jobs of same type.
+# 
+# 3. The test DB won't always have schema at this stage
+#    so don't try and enqueue delayed job in the test env
+
+if not ENV['RAILS_ENV'] == 'test'
+  Delayed::Job.enqueue PageCacheWarmingJob.new(1) # hardcoded account#1 
+end

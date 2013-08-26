64886c08c8c01f45319336c9308d3151cbc7a054
Shaun Robinson <shaun@squiid.com>
2013-08-25 21:09:05 -0400

    [migration] Uncomment previously commented condition. Shouldn't have commented an already-run migration, instead should have reversed the condition with a new migration.

--- db/migrate/20120731151504_add_account_references_on_most_models.rb
+++ db/migrate/20120731151504_add_account_references_on_most_models.rb
@@ -1,10 +1,10 @@
 class AddAccountReferencesOnMostModels < ActiveRecord::Migration
   def change
-    add_column :charts, :account_id, :integer#, :null => false
-    add_column :events, :account_id, :integer#, :null => false
-    add_column :artists, :account_id, :integer#, :null => false
-    add_column :tickets, :account_id, :integer#, :null => false
-    add_column :users, :account_id, :integer#, :null => false
-    add_column :orders, :account_id, :integer#, :null => false
+    add_column :charts, :account_id, :integer, :null => false
+    add_column :events, :account_id, :integer, :null => false
+    add_column :artists, :account_id, :integer, :null => false
+    add_column :tickets, :account_id, :integer, :null => false
+    add_column :users, :account_id, :integer, :null => false
+    add_column :orders, :account_id, :integer, :null => false
   end
 end

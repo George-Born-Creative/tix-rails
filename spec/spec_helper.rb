fb9d251a5ca3182cda2cc338b491178c38f4f379
Shaun Robinson <shaun@squiid.com>
2013-08-25 22:54:17 -0400

    [spec] Set both rails and rack envs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
-ENV["RAILS_ENV"] = 'test'
-ENV['RACK_ENV'] = 'test'
+ENV["RAILS_ENV"] = ENV["RACK_ENV"] = 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
